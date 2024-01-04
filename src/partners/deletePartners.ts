b7e14e55118ba00c2b1f9e9c9edb6ea4e341960b
Jezerwel <jezerwel.grino-22@cpu.edu.ph>
2024-01-04 21:21:37 +0800

    feat(admin-api): implement partner deletion

---
+++ src/partners/deletePartners.ts
@@ -0,0 +1,16 @@
+import { Express } from "express";
+import { Pool } from "pg";
+
+export const deletePartners = (app: Express, db: Pool) => {
+
+ app.delete('/admin/delete/partners/:id', async (req, res) => {
+   const { id } = req.params;
+   try {
+     await db.query('DELETE FROM partners where id = $1', [id]);
+     res.status(200).send('Partner deleted successfully.')
+   } catch (error) {
+     console.error(error);
+     res.status(500).send('Server error.')
+   }
+ })
+}
