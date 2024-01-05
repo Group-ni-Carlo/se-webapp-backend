569e8c4d1592f1554d816bc34a690cee3ff73469
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-01-06 02:43:08 +0800

    build: added partners migration

---
+++ db/migrations/20240105184112_create_partners_table.sql
@@ -0,0 +1,14 @@
+-- migrate:up
+CREATE TABLE Partners(
+    id SERIAL,
+    title VARCHAR(60),
+    logo_file VARCHAR,
+    date_created TIMESTAMP
+    date_last_edit TIMESTAMP
+    admin_id INT
+    FOREIGN KEY (admin_id) REFERENCES Admins(id)
+    ON DELETE CASCADE
+)
+
+-- migrate:down
+DROP TABLE Partners
