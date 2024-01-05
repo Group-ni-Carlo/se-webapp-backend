217f23af4c2d461cedc95d8a10c8cf42d2672835
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-01-06 05:49:35 +0800

    fix: partners migration

--- db/migrations/20240105184112_create_partners_table.sql
+++ db/migrations/20240105184112_create_partners_table.sql
@@ -3,8 +3,8 @@ CREATE TABLE Partners(
     id SERIAL,
     title VARCHAR(60),
     logo_file VARCHAR,
-    date_created TIMESTAMP
-    date_last_edit TIMESTAMP
+    date_created TIMESTAMP,
+    date_last_edit TIMESTAMP,
     admin_id INT
     FOREIGN KEY (admin_id) REFERENCES Admins(id)
     ON DELETE CASCADE
