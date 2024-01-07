33985574eaf17dbafa976670b097e8f1ef146e05
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-01-08 00:30:39 +0800

    feat: add merch api

--- src/routes/merch.ts
+++ src/routes/merch.ts
@@ -2,11 +2,14 @@ import express, { IRouter } from 'express';
 
 import { databaseConnection as pool } from '../utils/pool';
 import { getMerch } from '../merch/getMerch';
+import { orderMerch } from '../merch/orderMerch';
 
 const router: IRouter = express.Router();
 
 router.use(express.static('uploads'));
 
 getMerch(router, pool);
+
+orderMerch(router, pool);
 
 export default router;
